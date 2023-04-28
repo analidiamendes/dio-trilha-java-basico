d758226fca1f4639741b7a425fd2fd6092eedd18
Ana Lidia <alves.analidia@hotmail.com>
2023-04-27 21:39:56 -0300

    Add codigo na classe TiposVariaveis e criação da classe Operadores

---
+++ src/tipos_variaveis/Operadores.java
@@ -0,0 +1,29 @@
+package tipos_variaveis;
+
+public class Operadores {
+
+	public static void main(String[] args) {
+		
+		/*String nomeCompleto = "LINGUAGEM " + "JAVA";
+		System.out.println(nomeCompleto);*/
+		
+		String concatenacao = "?";
+		
+		concatenacao = 1+1+1+"1";
+		System.out.println(concatenacao);
+		
+		concatenacao = 1+"1"+1+1;
+		System.out.println(concatenacao);
+		
+		concatenacao = 1+"1"+1+"1";
+		System.out.println(concatenacao);
+		
+		concatenacao = "1"+1+1+1;
+		System.out.println(concatenacao);
+		
+		concatenacao = "1"+(1+1+1);
+		System.out.println(concatenacao);
+
+	}
+
+}
--- src/tipos_variaveis/TiposVariaveis.java
+++ src/tipos_variaveis/TiposVariaveis.java
@@ -3,6 +3,21 @@ package tipos_variaveis;
 public class TiposVariaveis {
 
 	public static void main(String[] args) {
+		
+		String meuNome = "Ana Lídia";
+		
+		double salarioMinino = 2500;
+		
+		short numeroCurto = 1;
+		int numeroNormal = numeroCurto;
+		short numeroCurto2 = (short) numeroNormal;
+		
+		int numero= 5;		
+		numero = 10;		
+		System.out.println(numero);
+		
+		final double VALOR_DE_PI = 3.14;	
+		
 
 	}
 
